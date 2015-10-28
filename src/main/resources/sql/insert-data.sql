812130c63482a886adb0e18aaef0e896ad77f2cd
Vadim <Vadim@home-pc.lan>
2015-10-28 07:10:30 +0200

    part of game

--- src/main/resources/sql/insert-data.sql
+++ src/main/resources/sql/insert-data.sql
@@ -1,6 +1,6 @@
 INSERT INTO login_details (  id, username, password , account_non_expired, account_non_locked, credentials_non_expired, enabled) VALUES  ( 1, 'dealer', 'sasa',  true, true, true, false);
 INSERT INTO login_authorities (  id, username, authority ) VALUES  ( 1, 'dealer', 'ROLE_DEALER');
-INSERT INTO accounts (  id, username, a_number,  amount) VALUES  ( 1, 'dealer', '307307307', 30);
+INSERT INTO accounts (  id, username, a_number,  amount) VALUES  ( 1, 'dealer', '307307307', 1000);
 
 INSERT INTO login_details (  id, username, password , account_non_expired, account_non_locked, credentials_non_expired, enabled) VALUES  ( 2, 'LuckyBoy', '123',  true, true, true, true);
 INSERT INTO login_authorities (  id, username, authority ) VALUES  ( 2, 'LuckyBoy', 'ROLE_USER');
